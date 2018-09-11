deca69317a1d209131160e20cb3b24bde99d026a
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2018-09-11 10:44:47 -0500

    Requesting Permissions.

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import RNSimData from "react-native-sim-data";
+import { PermissionsAndroid } from 'react-native'
+
+PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE)
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Getting Phone Number with React Native</Text>
+        <Text style={styles.data}>{JSON.stringify(RNSimData.getSimInfo())}</Text>
       </View>
     );
   }
@@ -20,6 +25,11 @@ const styles = StyleSheet.create({
   },
   welcome: {
     fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  data: {
+    fontSize: 18,
     textAlign: 'center',
     margin: 10,
   }
