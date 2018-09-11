2b98f5f30b7e896d39bd6c540ff6438d36e6a701
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2018-09-11 11:06:26 -0500

    Destructuring data.

--- App.js
+++ App.js
@@ -1,16 +1,65 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { PermissionsAndroid, StyleSheet, Text, View } from 'react-native';
 import RNSimData from "react-native-sim-data";
-import { PermissionsAndroid } from 'react-native'
 
-PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE)
+const initialState = {
+  data: {}
+};
 
 export default class App extends Component {
+  state = initialState;
+
+  componentDidMount() {
+    this.requestPermissions();
+    this.getSimData();
+  }
+
+  getSimData() {
+    const data = RNSimData.getSimInfo();
+
+    this.setState({ data });
+  }
+
+  /**
+   * It will be execute just the first time requesting the permission.
+   */
+  requestPermissions() {
+    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE)
+  }
+
   render() {
+    const {
+      isNetworkRoaming0,
+      mcc0,
+      subscriptionId0,
+      simSerialNumber0,
+      carrierName0,
+      isDataRoaming0,
+      phoneNumber0,
+      mnc0,
+      simSlotIndex0,
+      deviceId0,
+      displayName0,
+      countryCode0
+    } = this.state.data;
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Getting Phone Number with React Native</Text>
-        <Text style={styles.data}>{JSON.stringify(RNSimData.getSimInfo())}</Text>
+        <View style={styles.data}>
+          <Text>isNetworkRoaming: {JSON.stringify(isNetworkRoaming0)}</Text>
+          <Text>Mcc: {mcc0}</Text>
+          <Text>subscriptionId: {subscriptionId0}</Text>
+          <Text>simSerialNumber: {simSerialNumber0}</Text>
+          <Text>carrierName: {carrierName0}</Text>
+          <Text>isDataRoaming: {JSON.stringify(isDataRoaming0)}</Text>
+          <Text>phoneNumber: {phoneNumber0}</Text>
+          <Text>mnc: {mnc0}</Text>
+          <Text>simSlotIndex: {simSlotIndex0}</Text>
+          <Text>deviceId: {deviceId0}</Text>
+          <Text>displayName: {displayName0}</Text>
+          <Text>countryCode: {countryCode0}</Text>
+        </View>
       </View>
     );
   }
